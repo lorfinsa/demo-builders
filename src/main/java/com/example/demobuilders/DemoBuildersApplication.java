141054cb0509ae13efd6bbee55c2bb5ce8bac808
Jonathan Buzzetti <buzzetti.jp@gmail.com>
2021-01-11 15:27:21 -0300

    creacion de proyecto vacio

---
+++ src/main/java/com/example/demobuilders/DemoBuildersApplication.java
@@ -0,0 +1,13 @@
+package com.example.demobuilders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBuildersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBuildersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demobuilders/DemoBuildersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demobuilders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBuildersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
